bb7cef4726e8ae043e48e7096b654dda76dc31b8
Andrey Groza <andrey.groza@rutorika.ru>
2018-03-26 18:16:43 +0300

    Add navigation and first screen js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
+$(document).ready(function () {
 
+
+    let resizeFirstScreen = function() {
+
+        // Если размер экрана больше собственной высоты блока, увеличиваем
+        let currentHeight = $('.first-page').height();
+        let windowHeight = $(window).height();
+
+        if (windowHeight > currentHeight) {
+            $('.first-page').height($(window).height());
+        }
+
+    };
+
+    resizeFirstScreen();
+
+    $('.nav__link').click(function(e) {
+
+        e.preventDefault();
+
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 100 } , 500);
+
+    });
+
+});
