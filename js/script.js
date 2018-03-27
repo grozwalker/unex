304272ebc4c9058d2369136ffef041ab94f0d444
Andrey Groza <grozwalker@gmail.com>
2018-03-28 01:17:54 +0300

    Add form

--- js/script.js
+++ js/script.js
@@ -15,12 +15,27 @@ $(document).ready(function () {
 
     resizeFirstScreen();
 
-    $('.nav__link').click(function(e) {
+    $('a.docs-fancy').fancybox();
 
+    $("#phone").mask("+7 (999) 99-99-999");
+
+    $('.nav__link').click(function(e) {
         e.preventDefault();
 
         $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 100 } , 500);
+    });
 
+    $('.btn_type_consultation').click(function(e) {
+        e.preventDefault();
+
+        $('#request').toggle('500');
+
+    });
+
+    $('.close').click(function(e) {
+        e.preventDefault();
+
+        $('#request').toggle('500');
     });
 
 });
